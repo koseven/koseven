b281407660117983edbb8bec6e7fe7f614653290
Anton <antonfedonjuk@gmail.com>
2018-12-01 21:24:54 +0300

    Update I18n.php

--- system/classes/Kohana/I18n.php
+++ system/classes/Kohana/I18n.php
@@ -7,10 +7,12 @@
  * function, which loads the message and replaces parameters:
  *
  *     // Display a translated message
+ *     echo I18n::get('Hello, world');
  *     echo __('Hello, world');
  *
  *     // With parameter replacement
- *     echo __('Hello, :user', array(':user' => $username));
+ *     echo __('Hello, :user', [':user' => $username]);
+ *     echo I18n::get(['Hello, :user', [':user' => $username]]);
  *
  * @package    Kohana
  * @category   Base
@@ -44,13 +46,12 @@ class Kohana_I18n {
 	 *     // Change the current language to Spanish
 	 *     I18n::lang('es-es');
 	 *
-	 * @param   string  $lang   new language setting
+	 * @param   string  $lang  new language setting
 	 * @return  string
-	 * @since   3.0.2
 	 */
 	public static function lang($lang = NULL)
 	{
-		if ($lang)
+		if ($lang AND $lang !== I18n::$lang)
 		{
 			// Normalize the language
 			I18n::$lang = strtolower(str_replace([' ', '_'], '-', $lang));
@@ -61,27 +62,43 @@ class Kohana_I18n {
 
 	/**
 	 * Returns translation of a string. If no translation exists, the original
-	 * string will be returned. No parameters are replaced.
+	 * string will be returned.
 	 *
-	 *     $hello = I18n::get('Hello friends, my name is :name');
+	 *     $str = I18n::get('Hello friends, my name is Alex');
+	 *     $str = I18n::get(['Hello friends, my name is :name', [':name' => 'Alex']]);
 	 *
-	 * @param   string  $string text to translate
-	 * @param   string  $lang   target language
+	 * @param   string|array  $string  text to translate or array(text, values to replace in text)
+	 * @param   string        $lang    target language
 	 * @return  string
 	 */
 	public static function get($string, $lang = NULL)
 	{
+		$values = [];
+
+		if (is_array($string))
+		{
+			if (isset($string[1]) AND is_array($string[1]))
+			{
+				$values = $string[1];
+			}
+			$string = (string) $string[0];
+		}
+
 		if ( ! $lang)
 		{
 			// Use the global target language
 			$lang = I18n::$lang;
 		}
 
-		// Load the translation table for this language
-		$table = I18n::load($lang);
+		if ($lang !== I18n::$lang)
+		{
+			// Load the translation table for this language
+			$table = I18n::load($lang);
+			// Return the translated string if it exists
+			$string = isset($table[$string]) ? $table[$string] : $string;
+		}
 
-		// Return the translated string if it exists
-		return isset($table[$string]) ? $table[$string] : $string;
+		return empty($values) ? $string : strtr($string, $values);
 	}
 
 	/**
@@ -124,43 +141,31 @@ class Kohana_I18n {
 				// files from overloading more specific files
 				$table += $t;
 			}
-
-			// Remove the last part
-			array_pop($parts);
 		}
-		while ($parts);
+		while (array_pop($parts));
 
 		// Cache the translation table locally
 		return I18n::$_cache[$lang] = $table;
 	}
-
 }
 
 if ( ! function_exists('__'))
 {
 	/**
-	 * Kohana translation/internationalization function. The PHP function
-	 * [strtr](http://php.net/strtr) is used for replacing parameters.
+	 * Kohana translation/internationalization function.
 	 *
-	 *    __('Welcome back, :user', array(':user' => $username));
+	 *    $str = __('Welcome back, :user', [':user' => $username]);
 	 *
 	 * [!!] The target language is defined by [I18n::$lang].
 	 *
 	 * @uses    I18n::get
 	 * @param   string  $string text to translate
 	 * @param   array   $values values to replace in the translated text
-	 * @param   string  $lang   source language
+	 * @param   string  $lang   target language
 	 * @return  string
 	 */
-	function __($string, array $values = NULL, $lang = 'en-us')
+	function __($string, array $values = NULL, $lang = NULL)
 	{
-		if ($lang !== I18n::$lang)
-		{
-			// The message and target languages are different
-			// Get the translation for this message
-			$string = I18n::get($string);
-		}
-
-		return empty($values) ? $string : strtr($string, $values);
+		return I18n::get($values ? [$string, $values] : $string, $lang);
 	}
 }
