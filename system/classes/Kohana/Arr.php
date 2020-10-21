b075fae4fc58eb2d777a6c01b299e1b8e86b326e
ant0xa <semenov-anton-a@users.noreply.github.com>
2020-10-21 18:02:57 +0300

    ErrorException [ Deprecated ]:
    ErrorException [ Deprecated ]: array_key_exists(): Using array_key_exists() on objects is deprecated. Use isset() or property_exists() instead

--- system/classes/Kohana/Arr.php
+++ system/classes/Kohana/Arr.php
@@ -101,7 +101,7 @@ class Kohana_Arr {
 		}
 		else
 		{
-			if (array_key_exists($path, $array))
+			if (isset($path, $array))
 			{
 				// No need to do extra processing
 				return $array[$path];
